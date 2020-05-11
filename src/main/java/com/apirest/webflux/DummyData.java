071f62b301edd306916bede055fdf434a748621f
stephanogiuseppe <stephano.grp@gmail.com>
2020-05-10 23:06:42 -0300

    simple create and read playlist api

---
+++ src/main/java/com/apirest/webflux/DummyData.java
@@ -0,0 +1,31 @@
+package com.apirest.webflux;
+
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.repository.PlaylistRepository;
+
+import reactor.core.publisher.Flux;
+
+// @Component
+public class DummyData implements CommandLineRunner {
+	private final PlaylistRepository playlistRepository;
+	
+	public DummyData(PlaylistRepository playlistRepository) {
+		this.playlistRepository = playlistRepository;
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		playlistRepository
+			.deleteAll()
+			.thenMany(
+				Flux.just("First playlist", "Second playlist", "Nth playlist", "Best playlist")
+					.map(playlistName -> new Playlist(UUID.randomUUID().toString(), playlistName))
+					.flatMap(playlistRepository::save)
+			).subscribe(System.out::println);
+	}
+}
---
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -0,0 +1,39 @@
+package com.apirest.webflux.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.services.PlaylistService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(value = "/api")
+public class PlaylistController {
+
+	@Autowired
+	PlaylistService playlistService;
+	
+	@GetMapping(value = "/playlists")
+	public Flux<Playlist> getPlaylist() {
+		return playlistService.findAll();
+	}
+	
+	@GetMapping(value = "/playlist/{id}")
+	public Mono<Playlist> getPlaylistById(@PathVariable String id) {
+		return playlistService.findById(id);
+	}
+	
+	@PostMapping(value = "/playlist")
+	public Mono<Playlist> savePlaylist(@RequestBody Playlist playlist) {
+		return playlistService.save(playlist);
+	}
+	
+}
